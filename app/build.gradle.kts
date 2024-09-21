5562f6d9d72b3b7bed73c9de0b593ff7b967ebf0
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-21 10:44:17 +0300

    updated: snack bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlinsnackbar"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlinsnackbar"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlinsnackbar/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinsnackbar/MainActivity.kt
@@ -1,13 +1,47 @@
 package com.ismailmesutmujde.kotlinsnackbar
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.ismailmesutmujde.kotlinsnackbar.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
+
+        bindingMain.buttonNormal.setOnClickListener { view ->
+            Snackbar.make(view, "Hello", Snackbar.LENGTH_SHORT).show()
+        }
+
+        bindingMain.buttonReturn.setOnClickListener { x ->
+            Snackbar.make(x, "Delete the message?", Snackbar.LENGTH_SHORT)
+                .setAction("YES") { y ->
+                    Snackbar.make(y, "Message deleted.", Snackbar.LENGTH_SHORT).show()
+                }
+                .show()
+
+        }
+
+        bindingMain.buttonPrivate.setOnClickListener { z ->
+            val snackBar = Snackbar.make(z, "No internet connection!", Snackbar.LENGTH_LONG)
+            snackBar.setAction("Try Again") {
+
+            }
+
+            snackBar.setActionTextColor(Color.RED)
+            snackBar.setTextColor(Color.BLUE)
+            snackBar.setBackgroundTint(Color.LTGRAY)
+            snackBar.show()
+
+        }
 
     }
 }
